1da9832bdbade3cd9988c4be3edf10fe79b7536c
Ratimir <ra0953029364@gmail.com>
2019-12-04 06:46:01 +0200

    Добавьте файлы проекта.

---
+++ exercise_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово!");
+            String word = Console.ReadLine();
+            switch (word)
+            {
+                case "мяу":
+                    Console.WriteLine("Покорми кота!");
+                    break;
+                case "гав":
+                    Console.WriteLine("Погуляй с собакой!");
+                    break;
+                default:
+                    Console.WriteLine("я не знаю таких животных");
+                    break;
+            }
+        }
+    }
+}
